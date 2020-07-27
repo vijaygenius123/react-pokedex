94e035f70cc76268e17b0bc5c379f622480d8442
Vijayaraghavan Sundararaman <vijaygenius123@gmail.com>
2020-07-27 22:05:12 +0530

    Setup Routing

--- src/index.js
+++ src/index.js
@@ -4,9 +4,16 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+import { Router } from 'react-router-dom';
+import { createBrowserHistory } from 'history'
+
+const history = createBrowserHistory()
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router history={history} >
+      <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
